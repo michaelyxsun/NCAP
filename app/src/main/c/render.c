21cfa3bd4ee6eb22efb981b2f4f6459c31ac2191
M-Y-Sun <sun.m.yixuan@gmail.com>
2025-06-16 16:55:43 -0700

    make rendering shapes in render.c more systematic and add close button that does nothing

--- app/src/main/c/render.c
+++ app/src/main/c/render.c
@@ -14,7 +14,118 @@ bool            render_ready = false;
 pthread_mutex_t render_mx    = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t  render_cv    = PTHREAD_COND_INITIALIZER;
 
-static const int FPS = 30;
+static const int FPS     = 30;
+static const int FONTSIZ = 50;
+
+/**
+ * 0: close text background
+ * 1: close text
+ * 2: prompt text
+ */
+static struct obj_t objs[MAX_OBJS];
+static size_t       objs_len;
+
+static void
+init_objs (const int SCW, const int SCH)
+{
+    static char           buf_[OBJ_BUF_SIZ];
+    char                 *buf = buf_;
+    int                   pos = 0;
+    int                   x, y, w, h, pad, add;
+    struct rl_text_arg_t *textarg;
+    struct rl_rect_arg_t *rectarg;
+
+    // close text
+
+    static struct rl_text_arg_t objs1;
+    textarg = objs[1].params = &objs1;
+    objs[1].typ              = RL_TEXT;
+    objs[1].dyn              = false;
+
+    textarg->str  = "close";
+    textarg->fsiz = FONTSIZ + 20;
+    w             = MeasureText (textarg->str, textarg->fsiz);
+    h             = textarg->fsiz;
+    x = textarg->x = SCW - w - 90;
+    y = textarg->y = 90;
+    textarg->color = WHITE;
+
+    // close text background (tappable)
+
+    static struct rl_rect_arg_t objs0;
+    rectarg = objs[0].params = &objs0;
+    objs[0].typ              = RL_RECT;
+    objs[0].dyn              = false;
+
+    rectarg->siz.x = w + 20;
+    rectarg->siz.y = h + 20;
+    rectarg->pos.x = x - 10;
+    rectarg->pos.y = y - 10;
+    rectarg->color = RED;
+
+    // prompt text
+
+    static struct rl_text_arg_t objs2;
+    textarg = objs[2].params = &objs2;
+    objs[2].typ              = RL_TEXT;
+    objs[2].dyn              = false;
+
+    pos += (add = snprintf (buf, sizeof buf_ - pos,
+                            "hello from raylib in %d x %d", SCW, SCH)
+                  + 1);
+
+    textarg->str   = buf;
+    textarg->fsiz  = FONTSIZ;
+    w              = MeasureText (buf, FONTSIZ);
+    textarg->x     = (SCW - w) >> 1;
+    textarg->y     = (SCH - FONTSIZ) >> 1;
+    textarg->color = BLACK;
+
+    buf += add + 1;
+
+    objs_len = 3;
+}
+
+static void
+draw (const struct obj_t *const obj)
+{
+    switch (obj->typ) {
+        case RL_LINE:
+            DrawLineV (((struct rl_line_arg_t *)obj->params)->v1,
+                       ((struct rl_line_arg_t *)obj->params)->v2,
+                       ((struct rl_line_arg_t *)obj->params)->color);
+            break;
+        case RL_CIRC:
+            DrawCircleV (((struct rl_circ_arg_t *)obj->params)->c,
+                         ((struct rl_circ_arg_t *)obj->params)->r,
+                         ((struct rl_circ_arg_t *)obj->params)->color);
+
+            break;
+        case RL_RECT:
+            DrawRectangleV (((struct rl_rect_arg_t *)obj->params)->pos,
+                            ((struct rl_rect_arg_t *)obj->params)->siz,
+                            ((struct rl_rect_arg_t *)obj->params)->color);
+
+            break;
+        case RL_TRI:
+            DrawTriangle (((struct rl_tri_arg_t *)obj->params)->v1,
+                          ((struct rl_tri_arg_t *)obj->params)->v2,
+                          ((struct rl_tri_arg_t *)obj->params)->v3,
+                          ((struct rl_tri_arg_t *)obj->params)->color);
+
+            break;
+        case RL_TEXT:
+            DrawText (((struct rl_text_arg_t *)obj->params)->str,
+                      ((struct rl_text_arg_t *)obj->params)->x,
+                      ((struct rl_text_arg_t *)obj->params)->y,
+                      ((struct rl_text_arg_t *)obj->params)->fsiz,
+                      ((struct rl_text_arg_t *)obj->params)->color);
+
+            break;
+        default:
+            break;
+    }
+}
 
 void
 render (void)
@@ -27,9 +138,7 @@ render (void)
     const int SCW = GetScreenWidth ();
     const int SCH = GetScreenHeight ();
 
-    const int FONTSIZ = 40;
-
-    char str[32];
+    char str[64];
     snprintf (str, sizeof str, "hello from raylib in %d x %d", SCW, SCH);
 
     logd ("%s: %s: Window dimensions: %d x %d", FILENAME, __func__, SCW, SCH);
@@ -42,8 +151,7 @@ render (void)
     pthread_cond_signal (&render_cv); // NOTE: change to broadcast when needed
     pthread_mutex_unlock (&render_mx);
 
-    const int POSX = (SCW >> 1) - (MeasureText (str, FONTSIZ) >> 1);
-    const int POSY = (SCH >> 1) - (FONTSIZ >> 1);
+    init_objs (SCW, SCH);
 
     static Vector2 tpos[MAX_TOUCH_POINTS];
 
@@ -54,7 +162,13 @@ render (void)
             BeginDrawing ();
             {
                 ClearBackground (WHITE);
-                DrawText (str, POSX, POSY, FONTSIZ, BLACK);
+
+                for (size_t i = 0; i < objs_len; ++i) {
+                    if (objs[i].dyn == true)
+                        continue;
+
+                    draw (&objs[i]);
+                }
             }
             EndDrawing ();
             continue;
@@ -79,7 +193,8 @@ render (void)
                           FONTSIZ, BLACK);
             }
 
-            DrawText (str, POSX, POSY, FONTSIZ, BLACK);
+            for (size_t i = 0; i < objs_len; ++i)
+                draw (&objs[i]);
         }
         EndDrawing ();
     }
--- app/src/main/c/render.h
+++ app/src/main/c/render.h
@@ -4,11 +4,62 @@
 #define RENDER_H
 
 #include <pthread.h>
+#include <raylib.h>
 #include <stdbool.h>
 
 #include <android_native_app_glue.h>
 
 extern struct android_app *GetAndroidApp (void);
+
+#define MAX_OBJS    32
+#define OBJ_BUF_SIZ 1024
+
+enum obj_typ_e {
+    RL_LINE,
+    RL_CIRC,
+    RL_RECT,
+    RL_TRI,
+    RL_TEXT,
+};
+
+struct rl_line_arg_t {
+    Vector2 v1;
+    Vector2 v2;
+    Color   color;
+};
+
+struct rl_circ_arg_t {
+    Vector2 c;
+    float   r;
+    Color   color;
+};
+
+struct rl_rect_arg_t {
+    Vector2 pos;
+    Vector2 siz;
+    Color   color;
+};
+
+struct rl_tri_arg_t {
+    Vector2 v1;
+    Vector2 v2;
+    Vector2 v3;
+    Color   color;
+};
+
+struct rl_text_arg_t {
+    char *str;
+    int   x;
+    int   y;
+    int   fsiz;
+    Color color;
+};
+
+struct obj_t {
+    void          *params;
+    enum obj_typ_e typ;
+    bool           dyn;
+};
 
 extern bool            render_ready;
 extern pthread_mutex_t render_mx;
